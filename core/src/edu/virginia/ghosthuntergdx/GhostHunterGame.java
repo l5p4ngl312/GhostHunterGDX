b98293eba168e387161f1341fe97a3f77499106c
l5p4ngl312 <l.5p4ngl312@gmail.com>
2014-11-04 23:29:30 -0500

    Created screen for actual game, testing commit from second computer

--- core/src/edu/virginia/ghosthuntergdx/GhostHunterGame.java
+++ core/src/edu/virginia/ghosthuntergdx/GhostHunterGame.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Game;
 public class GhostHunterGame extends Game {
 	public static final String TITLE = "Ghost Hunter GDX";
 	
+	//Test edit
 	@Override
 	public void create () {
 		setScreen(new Splash());
