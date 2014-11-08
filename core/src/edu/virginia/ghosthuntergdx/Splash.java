116ee6a75a4c2ce20e6b33c4cbb01e01c714eb55
drubin111 <drubin111@gmail.com>
2014-11-08 16:45:21 -0500

    splash screen and testlogo

--- core/src/edu/virginia/ghosthuntergdx/Splash.java
+++ core/src/edu/virginia/ghosthuntergdx/Splash.java
@@ -1,49 +1,70 @@
 package edu.virginia.ghosthuntergdx;
 
 import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
-public class Splash implements Screen{
+public class Splash implements Screen {
+
+	private Texture texture = new Texture(
+			Gdx.files.internal("img/testlogo.jpg"));
+	private Image splashImage = new Image(texture);
+	private Stage stage = new Stage();
+	private GhostHunterGame game;
+	private float time;
+
+	public Splash(GhostHunterGame g) {
+		this.game = g;
+	}
 
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
-		
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act();
+		stage.draw();
+		time += delta;
+		if (time > 5){
+			game.setScreen(new MainMenu(game));
+		}
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
-		
+		// splashImage.setX(5); //I don't know what the parameter should be
+		// splashImage.setY(5); //same as above
+		stage.addActor(splashImage);
+
 	}
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
-		
+		dispose();
+
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-		
+		texture.dispose();
+		stage.dispose();
+
 	}
 
 }
