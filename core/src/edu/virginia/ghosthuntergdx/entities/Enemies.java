cf55bf62079fb18b4278a6959ef29013744ce295
am7kg <am7kg@virginia.edu>
2014-11-11 15:31:21 -0500

    some work on enemies

--- core/src/edu/virginia/ghosthuntergdx/entities/Enemies.java
+++ core/src/edu/virginia/ghosthuntergdx/entities/Enemies.java
@@ -1,5 +1,44 @@
 package edu.virginia.ghosthuntergdx.entities;
 
-public class Enemies {
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
+public class Enemies extends PhysicsActor{
+
+	
+	Vector2 position;
+	Texture enemyTexture;
+	Rectangle bounds;
+	Player player;
+	double health;
+	
+	
+	
+	public Enemies(Vector2 position, Texture t, Player player) {
+		super(position, t);
+		this.position = position;
+		this.player = player;
+	}
+	
+	public void update(){
+		
+	
+		if(player.mBody.getPosition().x > position.x){
+		position.x++;
+	}
+		else{
+		position.x--;
+	}	
+		if(player.mBody.getPosition().y < position.y){
+		position.y++;
+	}
+		else{
+		position.y--;
+		}
+		
+	}
+	
 }
+	
+
---
+++ core/src/edu/virginia/ghosthuntergdx/entities/Ghost.java
@@ -0,0 +1,13 @@
+package edu.virginia.ghosthuntergdx.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+public class Ghost extends Enemies {
+
+	public Ghost(Vector2 position, Texture t, Player player) {
+		super(position, t, player);
+		
+	}
+
+}
