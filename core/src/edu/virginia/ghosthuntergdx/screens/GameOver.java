b66a9864de8c3d61a35f3d9294df2c768a5a9dbf
am7kg <am7kg@virginia.edu>
2014-11-30 22:55:10 -0500

    working on this game over screen
    not done but it shouldn't mess with anything

---
+++ core/src/edu/virginia/ghosthuntergdx/screens/GameOver.java
@@ -0,0 +1,192 @@
+package edu.virginia.ghosthuntergdx.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+import edu.virginia.ghosthuntergdx.entities.Player;
+
+public class GameOver implements Screen {
+
+	private Stage stage;
+	private TextureAtlas atlas, backAtlas;
+	private Skin skin;
+	private Table table;
+	private TextButton buttonMainMenu, buttonExit;
+	private BitmapFont redChiller, chillerFont;
+	private Label stats, killsH, zombieKillsH, ghostKillsH, shotsFiredH, artifactsFoundH;
+	public double kills, zombieKills, ghostKills, shotsFired, artifactsFound;
+
+	GhostHunterGame game;
+
+	public GameOver(GhostHunterGame ghg) {
+
+		this.game = ghg;
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		stage.act(delta);
+
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void show() {
+		stage = new Stage();
+
+		Texture back = new Texture("img/background.png");
+		TextureRegion backg = new TextureRegion(back);
+		TextureRegionDrawable background = new TextureRegionDrawable(backg);
+
+		Gdx.input.setInputProcessor(stage);
+
+		atlas = new TextureAtlas("ui/redButtons.pack");
+		skin = new Skin(atlas);
+
+		table = new Table(skin);
+		table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		// creating buttons
+		TextButtonStyle textButtonStyle = new TextButtonStyle();
+		textButtonStyle.up = skin.getDrawable("redButton.Up");
+		textButtonStyle.down = skin.getDrawable("redButton.Down");
+		textButtonStyle.pressedOffsetX = 1;
+		textButtonStyle.pressedOffsetY = -1;
+		textButtonStyle.font = chillerFont;
+
+		// creating fonts
+		redChiller = new BitmapFont(Gdx.files.internal("Font/redChiller.fnt"),
+				false);
+
+		chillerFont = new BitmapFont(
+				Gdx.files.internal("Font/chillerfont.fnt"), false);
+
+		buttonExit = new TextButton("EXIT", textButtonStyle);
+		buttonExit.addListener(new InputListener() {
+
+			@Override
+			public void enter(InputEvent event, float x, float y, int pointer,
+					Actor fromActor) {
+
+				super.enter(event, x, y, pointer, fromActor);
+				Gdx.app.exit();
+			}
+
+		});
+		buttonExit.pad(20);
+
+		buttonMainMenu = new TextButton("Main Menu", textButtonStyle);
+
+		buttonMainMenu.pad(20);
+
+		buttonMainMenu.addListener(new InputListener() {
+
+			@Override
+			public void enter(InputEvent event, float x, float y, int pointer,
+					Actor fromActor) {
+
+				super.enter(event, x, y, pointer, fromActor);
+				game.setScreen(new MainMenu(game));
+			}
+
+		});
+
+		// created headings for all the statistics 
+		LabelStyle headingStyle = new LabelStyle(redChiller, Color.WHITE);
+
+		stats = new Label("Statistics", headingStyle);
+		stats.setFontScale(2);
+		
+		artifactsFound = 1; // SPGame.getPlayer().getArtifacs();
+		String aF = Double.toString(artifactsFound);
+		artifactsFoundH = new Label("Statistics" + aF, headingStyle);
+		artifactsFoundH.setFontScale(2);
+		
+		ghostKills = 2; // SPGame.getPlayer().getGhostKills();
+		String gK = Double.toString(ghostKills);
+		ghostKillsH = new Label("Statistics" +gK, headingStyle);
+		ghostKillsH.setFontScale(2);
+		
+		kills = 3; //SPGame.getPlayer().getKills();
+		String k = Double.toString(kills);
+		killsH = new Label("Statistics" + k, headingStyle);
+		killsH.setFontScale(2);
+
+		shotsFired = 4; //SPGame.getPlayer().getShotsFired();
+		String sF = Double.toString(shotsFired);
+		shotsFiredH = new Label("Statistics" +sF, headingStyle);
+		shotsFiredH.setFontScale(2);
+		
+		
+		zombieKills = 5; //SPGame.getPlayer().getZombieKills();
+		String zK = Double.toString(zombieKills);
+		zombieKillsH = new Label("Statistics" + zK , headingStyle);
+		zombieKillsH.setFontScale(2);
+		
+		
+		table.setBackground(background);
+		table.add(stats).colspan(2);
+		table.row();
+		table.add(artifactsFoundH);
+		table.row();
+		table.add(ghostKillsH);
+		table.row();
+		table.add(shotsFiredH);
+		table.row();
+		table.add(zombieKillsH).spaceBottom(100);
+		table.add(buttonMainMenu).expandX();
+		table.add(buttonExit).expandX();
+		stage.addActor(table);
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
